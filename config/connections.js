efc9a6fec6ed436b2c0cd1d178f0edc7b9d23044
Justin Quinonez <justinqnns@gmail.com>
2018-09-24 18:58:25 -0400

    Heroku code JawsDB

--- config/connections.js
+++ config/connections.js
@@ -1,14 +1,18 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+var connection;
+if (process.eventNames.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+  mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "P3pperm!nt",
   database: "burgers_db"
 });
-
+}
 // Make connection.
 connection.connect(function(err) {
   if (err) {
